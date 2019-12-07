ab06d5974c3bc044f502ec9686c67c5f3f1ab5d9
britto <britto.gnanaraj@mrcooper.com>
2019-12-07 10:06:13 +0530

    Leaderboard RecyclerView Layout design

---
+++ app/src/main/java/com/gcc/smartcity/leaderboard/LeaderboardListAdapter.kt
@@ -0,0 +1,59 @@
+package com.gcc.smartcity.leaderboard
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.gcc.smartcity.R
+import com.gcc.smartcity.navigationdrawer.onRecyclerSelecetedListener
+
+class LeaderboardListAdapter(var leaderboardModelList: ArrayList<LeaderboardRecyclerViewModel> , var onRecyclerSelecetedListener: onRecyclerSelecetedListener) :
+    RecyclerView.Adapter<LeaderboardListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        // Inflate the custom layout
+        val menuView = inflater.inflate(R.layout.layout_list_item, parent, false)
+
+        // Return a new holder instance
+        return ViewHolder(menuView)
+
+    }
+
+    override fun getItemCount(): Int {
+        return leaderboardModelList.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val holderData = leaderboardModelList[position]
+
+//        val textView = viewHolder.menuName
+//        textView.setText(holderData.menuName)
+//        val ImageView = viewHolder.menuImage
+//        ImageView.setImageResource(holderData.imgResource)
+//        val view = viewHolder.dataView
+//        view.setOnClickListener(object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                onRecyclerSelecetedListener.onSelected(position)
+//            }
+//        })
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        var menuName: TextView = itemView.findViewById(R.id.lst_itm_menuname)
+        var menuImage: ImageView = itemView.findViewById(R.id.lst_itm_menuimg)
+        var dataView: LinearLayout = itemView.findViewById(R.id.lstView)
+
+        // Stores the itemView in a public final member variable that can be used
+        // to access the context from any ViewHolder instance.
+    }
+
+}
---
+++ app/src/main/java/com/gcc/smartcity/leaderboard/LeaderboardRecyclerViewModel.kt
@@ -0,0 +1,3 @@
+package com.gcc.smartcity.leaderboard
+
+data class LeaderboardRecyclerViewModel (val data : Any, val viewType: Int)
