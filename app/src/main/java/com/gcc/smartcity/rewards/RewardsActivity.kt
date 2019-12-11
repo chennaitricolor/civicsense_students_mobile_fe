26ce2fb11bce7b9c269edbeeda35bad2d616929c
Ajith Kumar K <ajithkumar.k@mrcooper.com>
2019-12-11 16:23:49 +0530

    Back button implemented

--- app/src/main/java/com/gcc/smartcity/rewards/RewardsActivity.kt
+++ app/src/main/java/com/gcc/smartcity/rewards/RewardsActivity.kt
@@ -2,6 +2,7 @@ package com.gcc.smartcity.rewards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.gcc.smartcity.R
@@ -9,13 +10,22 @@ import com.gcc.smartcity.R
 class RewardsActivity : AppCompatActivity() {
     private var rewardsListAdapter : RewardsListAdapter? = null
     private var rewardsRecyclerView : RecyclerView? = null
+    private var backArrowButton : ImageView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rewards)
         rewardsRecyclerView = findViewById(R.id.rewardsRecyclerView)
+        backArrowButton = findViewById(R.id.backArrowButton)
+        backArrowButton?.setOnClickListener {
+            goBack()
+        }
         setAdapter()
+    }
+
+    private fun goBack() {
+        this.onBackPressed()
     }
 
     private fun setAdapter() {
