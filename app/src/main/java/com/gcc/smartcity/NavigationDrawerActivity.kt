1aad1b7fd6028903bc1821f2f353c9c2fe2a53e8
britto <britto.gnanaraj@mrcooper.com>
2019-12-12 13:11:16 +0530

    import androidx recyclerview

--- app/src/main/java/com/gcc/smartcity/NavigationDrawerActivity.kt
+++ app/src/main/java/com/gcc/smartcity/NavigationDrawerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.gcc.smartcity.leaderboard.LeaderBoardActivity
 import com.gcc.smartcity.navigationdrawer.NavDrawerListAdapter
 import com.gcc.smartcity.navigationdrawer.NavDrawerListItem
