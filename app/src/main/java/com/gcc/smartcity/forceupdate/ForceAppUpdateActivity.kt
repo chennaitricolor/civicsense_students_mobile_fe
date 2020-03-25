88048c9063c9448e8878124cd193b0a916bfd85b
Ajith Kumar K <ajithkumar.k@mrcooper.com>
2020-03-25 23:53:41 +0530

    Added ForceUpdate Layouts

---
+++ app/src/main/java/com/gcc/smartcity/forceupdate/ForceAppUpdateActivity.kt
@@ -0,0 +1,13 @@
+package com.gcc.smartcity.forceupdate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gcc.smartcity.R
+
+class ForceAppUpdateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_force_app_update)
+    }
+}
