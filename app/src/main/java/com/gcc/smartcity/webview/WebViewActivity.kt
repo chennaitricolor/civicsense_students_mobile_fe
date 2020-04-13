2589e55eba3d517552b11d14bdf5c30e79087e7e
ajithpoison <ajithpoison@gmail.com>
2020-04-13 12:55:17 +0530

    Handled SSL error gracefully

--- app/src/main/java/com/gcc/smartcity/webview/WebViewActivity.kt
+++ app/src/main/java/com/gcc/smartcity/webview/WebViewActivity.kt
@@ -2,9 +2,9 @@ package com.gcc.smartcity.webview
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.http.SslError
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.webkit.SslErrorHandler
@@ -12,8 +12,11 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.gcc.smartcity.R
 import kotlinx.android.synthetic.main.activity_web_view.*
+
 
 class WebViewActivity : AppCompatActivity() {
 
@@ -38,6 +41,7 @@ class WebViewActivity : AppCompatActivity() {
         handlePullToRefresh()
         loadUrl(pageUrl)
     }
+
     private fun handlePullToRefresh() {
     }
 
@@ -50,10 +54,34 @@ class WebViewActivity : AppCompatActivity() {
         webView.webViewClient = object : WebViewClient() {
             override
             fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
-                handler?.proceed()
+                showDialogueForSSLError(handler, error)
             }
         }
     }
+
+    private fun showDialogueForSSLError(handler: SslErrorHandler?, error: SslError?) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        var message = "SSL Certificate error."
+        when (error!!.primaryError) {
+            SslError.SSL_UNTRUSTED -> message = "The certificate authority is not trusted."
+            SslError.SSL_EXPIRED -> message = "The certificate has expired."
+            SslError.SSL_IDMISMATCH -> message = "Mismatch in the certificate hostname."
+            SslError.SSL_NOTYETVALID -> message = "The certificate is not yet valid."
+        }
+        message += " Do you want to continue anyway?"
+
+        builder.setTitle("SSL Certificate Error")
+        builder.setMessage(message)
+        builder.setPositiveButton(
+            "continue"
+        ) { dialog, which -> handler!!.proceed() }
+        builder.setNegativeButton(
+            "cancel"
+        ) { dialog, which -> handler!!.cancel() }
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
     private fun setWebClient() {
         webView.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(
@@ -71,6 +99,7 @@ class WebViewActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         // Check if the key event was the Back button and if there's history
         if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
@@ -80,6 +109,7 @@ class WebViewActivity : AppCompatActivity() {
         // If it wasn't the Back key or there's no web page history, exit the activity)
         return super.onKeyDown(keyCode, event)
     }
+
     private fun loadUrl(pageUrl: String) {
         webView.loadUrl(pageUrl)
     }
